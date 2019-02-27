2163bb6dbeae1729f851f0c21499c61b2e6670bb
nassim <nassim.mouline@essorline.Com>
2019-02-27 12:53:34 +0100

    route adapted

--- src/views/partials/navigation.blade.php
+++ src/views/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 @if($ismaster)
 <ul>
-  <li><a href="/chatteradmin"><i class="fa fa-home"></i><span>Home</span></a></li>
+  <li><a href="{{route('admin.dashboard')}}"><i class="fa fa-home"></i><span>Dashboard</span></a></li>
   <li><a href="/chatteradmin/discussions"><i class="fa fa-list"></i><span>Discussions</span></a></li>
   <li><a href="/chatteradmin/categories"><i class="fa fa-archive"></i><span>Categories</span></a></li>
 </ul>

d0d900de949eca7ee8fb8c9275327d947484677a
nassim <nassim.mouline@essorline.Com>
2019-02-27 10:49:18 +0100

    removed replies navigations

--- src/views/partials/navigation.blade.php
+++ src/views/partials/navigation.blade.php
@@ -3,6 +3,5 @@
   <li><a href="/chatteradmin"><i class="fa fa-home"></i><span>Home</span></a></li>
   <li><a href="/chatteradmin/discussions"><i class="fa fa-list"></i><span>Discussions</span></a></li>
   <li><a href="/chatteradmin/categories"><i class="fa fa-archive"></i><span>Categories</span></a></li>
-  <li><a href="/chatteradmin/replies"><i class="fa fa-comments"></i><span>Replies</span></a></li>
 </ul>
 @endif
